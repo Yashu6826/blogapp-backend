c4fd1ee30a89ec3bb407de5df4c3a98c78e004b8
yashraj gupta <yashrajgupta6826@gmail.com>
2023-05-14 12:10:29 +0530

    yes

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const express = require('express');
-const cors = require("cors");
+
 const mongoose = require("mongoose");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+
 
 const dbUrl =
   process.env.MONGO_URL;
