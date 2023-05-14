9d679ef37ce92ced93af91e1676e6235db1404a0
yashraj gupta <yashrajgupta6826@gmail.com>
2023-05-14 13:48:15 +0530

    yes

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const authRoute = require("./routes/auth");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 const dbUrl =
   process.env.MONGO_URL;
