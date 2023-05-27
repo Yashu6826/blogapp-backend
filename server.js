e58fc40b81af625568d383cc175d7494ac68abe2
yashraj gupta <yashrajgupta6826@gmail.com>
2023-05-28 00:36:29 +0530

    yh

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ app.use(express.json());
 //   credentials:true,            //access-control-allow-credentials:true
 //   optionSuccessStatus:200
 // }
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
 
 const dbUrl =
   process.env.MONGO_URL;
